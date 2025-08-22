a9e2d2a7bc0d94efcd7d4cdabd65bbb6775bf5df
sudeep9 <ssjgit@gmail.com>
2025-08-22 17:17:13 +0100

    added js paths

--- pluginLoader.js
+++ pluginLoader.js
@@ -5,8 +5,8 @@ import {
     Get2Plugin,
     Delete2Plugin,
     Patch2Plugin
-} from 'datastar-default-actions.js';
-import {SignalsPlugin} from 'datastar-signals.js';
+} from '/datastar-plugins/datastar-default-actions.js';
+import {SignalsPlugin} from '/datastar-plugins/datastar-signals.js';
 
 import {load} from 'https://cdn.jsdelivr.net/gh/starfederation/datastar@main/bundles/datastar.js';
 
